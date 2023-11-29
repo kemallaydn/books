3b0f6e6b1fde191b1b48c74bade5f04135a0e1ba
kemallaydn <kemal.aydin3429@gmail.com>
2023-11-29 15:54:29 +0300

    bug fix

--- src/components/tab/index.tsx
+++ src/components/tab/index.tsx
@@ -3,8 +3,8 @@ import Container from "../container";
 import styles from "./style"
 import React, { useState } from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import Tab from "../../models/tab.model";
-const Tab:React.FC<Tab> = ({ label, onPress, isSelected }) => {
+import TabProps from "../../models/tab.model";
+const Tab:React.FC<TabProps> = ({ label, onPress, isSelected }) => {
   const tabItems=["home-sharp","heart-circle-outline","pencil-sharp","person-circle"]
   return (
     <View style={styles.container}>
